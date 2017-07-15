7ae9f14944934a6544bb54f0d20f0e223f127619
U-REDMOND\t-hshuan <t-hshuan@ZX4866321.redmond.corp.microsoft.com>
2017-07-14 23:42:31 -0700

    Fix bugs

--- drqa/layers.py
+++ drqa/layers.py
@@ -1,4 +1,4 @@
-`# Copyright (c) 2017-present, Facebook, Inc.
+# Copyright (c) 2017-present, Facebook, Inc.
 # All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
@@ -293,4 +293,3 @@ def weighted_avg(x, weights): # used in rnn_reader.py
         weights = batch * len
     """
     return weights.unsqueeze(1).bmm(x).squeeze(1)
-`
